6f746b988a2c7a2bde4cb8f19f725727d8afaaef
taro <taro@example.com>
2023-07-30 13:21:30 +0900

    update

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -38,6 +38,7 @@ import com.example.pictgram.entity.UserInf;
 import com.example.pictgram.form.TopicForm;
 import com.example.pictgram.form.UserForm;
 import com.example.pictgram.repository.TopicRepository;
+import com.example.pictgram.service.SendMailService;
 
 import java.util.Locale;
 import org.springframework.context.MessageSource;
@@ -47,6 +48,9 @@ import com.example.pictgram.form.FavoriteForm;
 
 import com.example.pictgram.entity.Comment;
 import com.example.pictgram.form.CommentForm;
+
+import org.thymeleaf.context.Context;
+import com.example.pictgram.service.SendMailService;
 
 @Controller
 public class TopicsController {
@@ -67,6 +71,9 @@ public class TopicsController {
 
 	@Value("${image.local:false}")
 	private String imageLocal;
+
+	@Autowired
+	private SendMailService sendMailService;
 
 	@GetMapping(path = "/topics")
 	public String index(Principal principal, Model model) throws IOException {
@@ -198,6 +205,12 @@ public class TopicsController {
 		redirAttrs.addFlashAttribute("class", "alert-info");
 		redirAttrs.addFlashAttribute("message",
 				messageSource.getMessage("topics.create.flash.2", new String[] {}, locale));
+
+		Context context = new Context();
+		context.setVariable("title", "【Pictgram】新規投稿");
+		context.setVariable("name", user.getUsername());
+		context.setVariable("description", entity.getDescription());
+		sendMailService.sendMail(context);
 
 		return "redirect:/topics";
 	}
